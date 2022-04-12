799d14aaa9669ba5e50df1ceefe3c199115194e5
Hirakok <105953979+Hirakok@users.noreply.github.com>
2022-04-12 08:51:15 +0000

    projects update

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -3,4 +3,8 @@ import { OpenAIStream, StreamingTextResponse } from "ai";
 
 // Create an OpenAI API client (that's edge friendly!)
 const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+// Set the runtime to edge for best performance
+export const runtime = "edge"
