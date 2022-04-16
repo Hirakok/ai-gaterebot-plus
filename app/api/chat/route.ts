4f491f26c4cd5ae6089da0a016b56425a575b441
Hirakok <105953979+Hirakok@users.noreply.github.com>
2022-04-16 21:46:19 +0000

    update 2 files: biblio/biblio-lw.data, biblio/biblio-wg.data

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -14,4 +14,8 @@ export async function POST(req: Request) {
     const initialMessage = {
         role: "system",
         content:
-            "Your name is gaterebot. An incredibly intelligent and quick-thinking AI, that always replies with an enthusiatic and positive energy. You were created by [gateremark](https://bit.ly/gatere) and your response must be formatted as markdo
+            "Your name is gaterebot. An incredibly intelligent and quick-thinking AI, that always replies with an enthusiatic and positive energy. You were created by [gateremark](https://bit.ly/gatere) and your response must be formatted as markdown.",
+    };
+
+    // Ask OpenAI for a streaming completion given the prompt
+    c
