c94cb69520c531687c4ffd0e2abcd1e664d5a4a0
Hirakok <105953979+Hirakok@users.noreply.github.com>
2022-04-19 22:04:39 +0000

    Forms

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -22,4 +22,6 @@ export async function POST(req: Request) {
         model: "gpt-3.5-turbo-1106",
         stream: true,
         // temperature: 0.6,
-        // ma
+        // max_tokens: 300,
+        messages: messages.concat(initialMessage),
+    
