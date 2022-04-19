8c6cb218c935ba39be201d2d8dc3ba97b737d3b9
Hirakok <105953979+Hirakok@users.noreply.github.com>
2022-04-19 17:45:24 +0000

    compressed

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -19,4 +19,7 @@ export async function POST(req: Request) {
 
     // Ask OpenAI for a streaming completion given the prompt
     const response = await openai.chat.completions.create({
-        model: 
+        model: "gpt-3.5-turbo-1106",
+        stream: true,
+        // temperature: 0.6,
+        // ma
