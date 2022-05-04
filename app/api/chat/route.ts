cf33d3a4d9948a8bbae3d5df6a4b1a40e0ef94fd
Hirakok <105953979+Hirakok@users.noreply.github.com>
2022-05-04 13:22:31 +0000

    fixed a syntax error

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -28,4 +28,5 @@ export async function POST(req: Request) {
     // Convert the response into a friendly text-stream
     const stream = OpenAIStream(response);
     // Respond with the stream
-   
+    return new StreamingTextResponse(stream);
+}
