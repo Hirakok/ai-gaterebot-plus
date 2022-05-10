5d6519e08090f83fb0abd6e05bfb736384e7a2df
Hirakok <105953979+Hirakok@users.noreply.github.com>
2022-05-10 06:59:50 +0000

    test log

--- app/chat/[[...chatId]]/page.tsx
+++ app/chat/[[...chatId]]/page.tsx
@@ -4,4 +4,6 @@ import { FormEvent } from "react";
 import ChatSidebar from "@/app/components/ChatSidebar";
 import { IoSend } from "react-icons/io5";
 import { useChat } from "ai/react";
-import Message from "@/app/comp
+import Message from "@/app/components/Message";
+import { useSession } from "next-auth/react";
+import Image from "next/
