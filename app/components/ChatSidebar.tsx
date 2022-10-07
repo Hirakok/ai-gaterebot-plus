3bead1d69eeeb12a58ab2ffa4996b2363b6847d9
Hirakok <105953979+Hirakok@users.noreply.github.com>
2022-10-07 11:59:03 +0000

    Fixed sorting

--- app/components/ChatSidebar.tsx
+++ app/components/ChatSidebar.tsx
@@ -2,4 +2,7 @@
 
 import { useSession, signOut } from "next-auth/react";
 import Image from "next/image";
-import { L
+import { LuLogOut } from "react-icons/lu";
+import { useState, useEffect } from "react";
+import Link from "next/link";
+impo
