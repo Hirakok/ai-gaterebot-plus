b01064042032819dc34d12d60549d315eaa37620
Hirakok <105953979+Hirakok@users.noreply.github.com>
2023-06-17 11:24:03 +0000

    loading indicator

--- app/components/Message.tsx
+++ app/components/Message.tsx
@@ -2,4 +2,7 @@
 
 import { useSession } from "next-auth/react";
 import Image from "next/image";
-import Markdown from "r
+import Markdown from "react-markdown";
+import rehypeHighlight from "rehype-highlight";
+
+interface 
