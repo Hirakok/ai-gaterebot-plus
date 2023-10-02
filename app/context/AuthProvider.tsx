e8698acb8007af928895e54482bbf3ec12cc0501
Hirakok <105953979+Hirakok@users.noreply.github.com>
2023-10-02 19:30:09 +0000

    fix exception handling

--- app/context/AuthProvider.tsx
+++ app/context/AuthProvider.tsx
@@ -7,4 +7,5 @@ export default function AuthProvider({
 }: {
     children: React.ReactNode;
 }) {
-    return <SessionProvider>{children}</Se
+    return <SessionProvider>{children}</SessionProvider>;
+}
