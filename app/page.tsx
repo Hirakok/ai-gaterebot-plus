70553d34c8850be4c01cf3ddfda42caa5c088f12
Hirakok <105953979+Hirakok@users.noreply.github.com>
2023-11-04 11:29:33 +0000

    Remove useless test

--- app/page.tsx
+++ app/page.tsx
@@ -51,4 +51,6 @@ export default function Home() {
                         href="/chat"
                         className="bg-[#ffffff] text-[#050816] p-2 md:p-3 rounded-lg hover:bg-[#646464] playpen cursor-pointer transition ease-in-out duration-500"
                     >
-               
+                        Continue to Chat
+                    </Link>
+                    <button
