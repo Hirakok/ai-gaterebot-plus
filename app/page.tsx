d61e324a12f068e3894eb1c28a2a6b5eaee57485
Hirakok <105953979+Hirakok@users.noreply.github.com>
2023-11-08 08:07:36 +0000

    Update .babelrc

--- app/page.tsx
+++ app/page.tsx
@@ -58,4 +58,9 @@ export default function Home() {
                         onClick={() => signOut()}
                     >
                         Sign Out
-                    </but
+                    </button>
+                </div>
+            </section>
+        </div>
+    );
+}
