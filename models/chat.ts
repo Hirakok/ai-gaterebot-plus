e3dce6e00739e476ccefc640a802a2724b873953
Hirakok <105953979+Hirakok@users.noreply.github.com>
2023-11-12 21:14:03 +0000

    New translations 03_p01_ch01_02.md (Urdu (India))

--- models/chat.ts
+++ models/chat.ts
@@ -1,3 +1,4 @@
 import mongoose, { Schema } from "mongoose";
 
-mongoose.connect(process.env
+mongoose.connect(process.env.MONGODB_URI ?? "MONGODB_URI is not defined");
+mongoose.Promise = global.Promi
