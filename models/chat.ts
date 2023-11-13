bb05c5d50fd0c3b35df6427aa3d32dc11add4396
Hirakok <105953979+Hirakok@users.noreply.github.com>
2023-11-13 09:37:22 +0000

    Fixing compilation error

--- models/chat.ts
+++ models/chat.ts
@@ -1,4 +1,10 @@
 import mongoose, { Schema } from "mongoose";
 
 mongoose.connect(process.env.MONGODB_URI ?? "MONGODB_URI is not defined");
-mongoose.Promise = global.Promi
+mongoose.Promise = global.Promise;
+
+const chatSchema = new Schema(
+    {
+        email: String,
+        title: String,
+  
