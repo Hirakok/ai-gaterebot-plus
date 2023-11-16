4af46ba3d72526faaf8ff0131c4ce34c3cb1512c
Hirakok <105953979+Hirakok@users.noreply.github.com>
2023-11-16 10:07:26 +0000

    Bump redcarpet from 3.5.0 to 3.5.1

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        remote
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: 
